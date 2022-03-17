b6946832e739fa84e7bffb5f21a1a5fe32ad6611
Graur <ancongr@yandex.ru>
2022-03-17 18:07:02 +0300

    hw06-oop: fix after discussion

--- hw06-oop/src/main/java/ru/otus/service/AtmService.java
+++ hw06-oop/src/main/java/ru/otus/service/AtmService.java
@@ -5,28 +5,18 @@ import ru.otus.data.Denomination;
 import ru.otus.exception.NotEnoughMoneyException;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AtmService implements ATM {
-    private final Map<Denomination, Integer> atmDenominations;
+    private final CashService cashService;
 
     public AtmService() {
-        this.atmDenominations = new LinkedHashMap<>();
-        this.atmDenominations.put(Denomination.FIVE_THOUSAND, 0);
-        this.atmDenominations.put(Denomination.THOUSAND, 0);
-        this.atmDenominations.put(Denomination.FIVE_HUNDRED, 0);
-        this.atmDenominations.put(Denomination.HUNDRED, 0);
-        this.atmDenominations.put(Denomination.TEN, 0);
+        this.cashService = new CashService();
     }
 
     @Override
     public void deposit(List<Denomination> denominations) {
-        denominations.forEach(denomination -> {
-            int count = atmDenominations.get(denomination);
-            atmDenominations.put(denomination, count + 1);
-        });
+        denominations.forEach(cashService::depositCash);
     }
 
     @Override
@@ -34,22 +24,12 @@ public class AtmService implements ATM {
         if (cash > checkBalance()) {
             throw new NotEnoughMoneyException("There is not enough money for this operation");
         }
-        List<Denomination> withdrawCash = new ArrayList<>();
-        for (Denomination denomination : atmDenominations.keySet())  {
-            while(cash >= denomination.getAmount()) {
-                cash -= denomination.getAmount();
-                withdrawCash.add(denomination);
-            }
-        }
-        return withdrawCash;
+        List<Denomination> withdrawCashList = new ArrayList<>();
+        return cashService.withdrawCash(cash, withdrawCashList);
     }
 
     @Override
     public int checkBalance() {
-        int balance = 0;
-        for (Denomination denomination : atmDenominations.keySet()) {
-            balance += denomination.getAmount() * atmDenominations.get(denomination);
-        }
-        return balance;
+        return cashService.getBalance();
     }
 }
--- hw06-oop/src/main/java/ru/otus/service/CashService.java
+++ hw06-oop/src/main/java/ru/otus/service/CashService.java
@@ -1,7 +1,44 @@
 package ru.otus.service;
 
+import ru.otus.data.Denomination;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CashService {
+    private final Map<Denomination, Integer> atmDenominations;
+
+    public CashService() {
+        this.atmDenominations = new LinkedHashMap<>();
+        this.atmDenominations.put(Denomination.FIVE_THOUSAND, 0);
+        this.atmDenominations.put(Denomination.THOUSAND, 0);
+        this.atmDenominations.put(Denomination.FIVE_HUNDRED, 0);
+        this.atmDenominations.put(Denomination.HUNDRED, 0);
+        this.atmDenominations.put(Denomination.TEN, 0);
+    }
 
 
+    public void depositCash(Denomination denomination) {
+        int count = atmDenominations.get(denomination);
+        atmDenominations.put(denomination, count + 1);
+    }
+
+    public List<Denomination> withdrawCash(int cash, List<Denomination> withdrawCashList) {
+        for (Denomination denomination : atmDenominations.keySet())  {
+            while(cash >= denomination.getAmount()) {
+                cash -= denomination.getAmount();
+                withdrawCashList.add(denomination);
+            }
+        }
+        return withdrawCashList;
+    }
+
+    public int getBalance() {
+        int balance = 0;
+        for (Denomination denomination : atmDenominations.keySet()) {
+            balance += denomination.getAmount() * atmDenominations.get(denomination);
+        }
+        return balance;
+    }
 }
