6152dfb1d944340a1841af936b61367803f865b8
graur <ancongr@yandex.ru>
2022-04-13 20:02:21 +0300

    hw08-io: first steps to make functionality work

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,28 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    private final String filename;
+    private final ObjectMapper objectMapper;
+
     public FileSerializer(String fileName) {
+        this.filename = fileName;
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        try {
+            objectMapper.writeValue(Paths.get(this.filename).toFile(), data);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
--- hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,49 @@
 package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String filename;
+    private final ObjectMapper objectMapper;
+
     public ResourcesFileLoader(String fileName) {
+        this.filename = fileName;
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
-        return null;
+        return parse(read());
+    }
+
+    private File read() {
+        URL resource = getClass().getClassLoader().getResource(this.filename);
+        if (resource == null) {
+            throw new FileProcessException("file not found");
+        } else {
+            try {
+                return new File(resource.toURI());
+            } catch (URISyntaxException e) {
+                throw new FileProcessException(e.getMessage());
+            }
+        }
+    }
+
+    private List<Measurement> parse(File file) {
+        List<Measurement> measurements;
+        try {
+            measurements = objectMapper.readerForListOf(Measurement.class).readValue(file);
+        } catch (IOException e) {
+            throw new FileProcessException(e.getMessage());
+        }
+        return measurements;
     }
 }
